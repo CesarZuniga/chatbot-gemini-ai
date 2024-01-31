4de73eb2cf6ee9851cc90cfd77e4d3bf22e25a20
Cesar Zuñiga <cesar.zuniga@mobiik.com>
2024-01-30 18:26:20 -0600

    add github link

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -2,10 +2,10 @@
   <q-card class=" row no-shadow" bordered>
     <q-card-section class="col-12 text-center q-pa-lg">
 
-      <q-btn icon="mdi-github" flat dense color="grey-8"></q-btn>
-      <q-btn icon="mdi-facebook" flat dense color="grey-8"></q-btn>
+      <q-btn @click="openGithub" icon="mdi-github" flat dense color="grey-8"></q-btn>
+      <!-- <q-btn icon="mdi-facebook" flat dense color="grey-8"></q-btn>
       <q-btn icon="mdi-twitter" flat dense color="grey-8"></q-btn>
-      <q-btn icon="mdi-instagram" flat dense color="grey-8"></q-btn>
+      <q-btn icon="mdi-instagram" flat dense color="grey-8"></q-btn> -->
 
 
       <div class="text-body1 q-mt-sm text-grey-8 text-weight-bold">
@@ -16,6 +16,9 @@
 </template>
 
 <script setup>
+function openGithub() {
+  window.open('https://github.com/CesarZuniga/chatbot-gemini-ai')
+}
 </script>
 
 <style scoped></style>
